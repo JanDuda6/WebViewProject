f757f01e1925df37e68823d5aa02a3aac7a126d9
Jan Duda <jan.duda6@gmail.com>
2025-07-08 20:42:01 +0200

    first commit

---
+++ WebViewProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebViewProject
+//
+//  Created by Jan Duda on 08/07/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
